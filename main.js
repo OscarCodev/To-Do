300293826878a30589ac057e1dd74cb52b6c75cc
OscarCodev <OscarCodev@users.noreply.github.com>
2023-03-20 09:50:10 -0500

    Mapear todos los elementos del DOM a manipular

---
+++ main.js
@@ -0,0 +1,15 @@
+//Obteniendo las listas
+const lista1 = document.querySelector("#lista1")
+const lista2 = document.querySelector("#lista2")
+const lista3 = document.querySelector("#lista3")
+//Obteniendo el input 
+const input = document.querySelector("#input")
+//Obteniendo las opciones de tipo radio
+const opciones = document.querySelectorAll('input[type=radio]');
+//Obteniendo el boton de agregar 
+const botonAgregar = document.querySelector("#boton-agregar")
+
+
+
+
+
