3edd2d12187add0b4edc85858255de559d8e1d76
OscarCodev <OscarCodev@users.noreply.github.com>
2023-03-21 07:53:03 -0500

    Agregar funcionalidad tarea realizada y tarea eliminada

--- main.js
+++ main.js
@@ -8,13 +8,17 @@ const input = document.querySelector("#input")
 const opciones = document.querySelectorAll('input[type=radio]');
 //Obteniendo el boton de agregar 
 const botonAgregar = document.querySelector("#boton-agregar")
-
+//Obteniendo la etiqueta de la fecha
+const fecha = document.querySelector('#fecha')
 
 //Obteniendo modificaciones de estilo
 const check = 'fa-check-circle'
 const uncheck = 'fa-circle'
 const lineThrough = 'line-through' //clase que ya esta definida en CSS
 
+//Creacion de fecha
+const FECHA = new Date()
+fecha.innerHTML = FECHA.toLocaleDateString('es-PE',{weekday:'long', month: 'short', day: 'numeric'})
 
 function obtenerOpcion(op){
     let opcionSeleccionada
@@ -53,8 +57,23 @@ function agregarTarea(tarea, proyecto, id, realizado, eliminado){
     }
 }
 
+//funcion cuando se marca una tarea realizada
+function tareaRealizada(element){
+    //Cambiar el diseño visual del circulo
+    element.classList.toggle(check)
+    element.classList.toggle(uncheck)
 
-//Eventos 
+    //Cambiar el diseño visual del texto a tachado 
+    element.parentNode.querySelector('.text').classList.toggle(lineThrough)
+}
+
+//funcion cuando se elimina una tarea
+function tareaEliminada(element){
+    element.parentNode.parentNode.removeChild(element.parentNode)
+}
+
+
+//Eventos  para agregar tareas
 botonAgregar.addEventListener('click', ()=>{
     const tarea = input.value
     const opcionSelect = obtenerOpcion(opciones)//Detectar que input radio esta seleccionado - retorna un string
@@ -64,4 +83,53 @@ botonAgregar.addEventListener('click', ()=>{
     input.value = ""
 })
 
+document.addEventListener('keydown', (event)=>{
+    if(event.key == 'Enter'){
+        const tarea = input.value
+        const opcionSelect = obtenerOpcion(opciones)
+        if(opcionSelect){
+            if(tarea){
+                agregarTarea(tarea, opcionSelect, 1, false, false)
+               }
+               input.value = ""
+        } 
+    }
+})
 
+//Eventos para controlar las tareas realizadas o las eliminadas
+
+lista1.addEventListener('click', function(event){
+    //Las dos primeras lineas basicamente leen el atributo data del elemento seleccionado dentro del ul lista
+    const element = event.target //La propiedad "target" del objeto "event" se refiere al elemento en el que se hizo clic
+    const elementData = element.attributes.data.value
+
+    if(elementData === 'realizado'){
+        tareaRealizada(element)
+    }else if (elementData === 'eliminado'){
+        tareaEliminada(element)
+    }
+})
+
+lista2.addEventListener('click', function(event){
+    //Las dos primeras lineas basicamente leen el atributo data del elemento seleccionado dentro del ul lista
+    const element = event.target //La propiedad "target" del objeto "event" se refiere al elemento en el que se hizo clic
+    const elementData = element.attributes.data.value
+
+    if(elementData === 'realizado'){
+        tareaRealizada(element)
+    }else if (elementData === 'eliminado'){
+        tareaEliminada(element)
+    }
+})
+
+lista3.addEventListener('click', function(event){
+    //Las dos primeras lineas basicamente leen el atributo data del elemento seleccionado dentro del ul lista
+    const element = event.target //La propiedad "target" del objeto "event" se refiere al elemento en el que se hizo clic
+    const elementData = element.attributes.data.value
+
+    if(elementData === 'realizado'){
+        tareaRealizada(element)
+    }else if (elementData === 'eliminado'){
+        tareaEliminada(element)
+    }
+})
